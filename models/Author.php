278bbfebece51951d4699e0d808094d9edfe231e
sersyol <sersy@mail.ru>
2024-07-15 21:24:46 +0300

    Генерация контроллеров и виевс книг и авторов

---
+++ models/Author.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "authors".
+ *
+ * @property int $id
+ * @property string $name
+ *
+ * @property Books[] $books
+ */
+class Author extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'authors';
+    }
+    // models/Author.php
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+        ];
+    }
+
+    /**
+     * Gets query for [[Books]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBooks()
+    {
+        return $this->hasMany(Books::class, ['author_id' => 'id']);
+    }
+}
