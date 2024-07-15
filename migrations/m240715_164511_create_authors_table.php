78dc002e0a4d8eda8c24c65708942ae5e1f705ba
sersyol <sersy@mail.ru>
2024-07-15 19:46:35 +0300

    Новый проект книги

---
+++ migrations/m240715_164511_create_authors_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%authors}}`.
+ */
+class m240715_164511_create_authors_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%authors}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(255)->notNull(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%authors}}');
+    }
+}
