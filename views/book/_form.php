89368b481cb68554b6c1dd6959e7fafb233c4ab6
sersyol <sersy@mail.ru>
2024-07-16 18:38:28 +0300

    Добавление кнопок авторов и книг

--- views/book/_form.php
+++ views/book/_form.php
@@ -2,7 +2,9 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
+use yii\helpers\ArrayHelper;
+use app\models\Author;
+use kartik\select2\Select2;
 /** @var yii\web\View $this */
 /** @var app\models\Book $model */
 /** @var yii\widgets\ActiveForm $form */
@@ -14,7 +16,18 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'author_id')->textInput() ?>
+    <?= $form->field($model, 'author_id')->textInput    () ?>
+
+    <div class="row">
+	<div class="col-md-4">
+                    <?= $form->field($model,
+                        'paymentterms_id')
+                        ->widget(Select2::class, [
+                            'data' => ArrayHelper::map(Author::find()->all(), 'id', 'name'),
+                            'options' => ['placeholder' => 'Выберите автора'],
+                        ]) ?>
+                </div>
+	 </div>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
